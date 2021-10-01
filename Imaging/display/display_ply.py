e9d13a363ff88cc42e0291371b8e8005179f7a26
Peter Holm <peter@l-holm.dk>
2021-10-01 10:59:23 +0200

    pcl 2 png

--- Imaging/display/display_ply.py
+++ Imaging/display/display_ply.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-#import numpy as np
+import numpy as np
 import open3d as o3d
+from matplotlib import pyplot as plt
 
 #file = "testdata/Crankshaft_HD.ply"
 file = "imaging/display/testdata/image8.ply"
@@ -18,11 +19,11 @@ def show(pcd):
 
 def show2(pcd):
     o3d.visualization.draw_geometries([pcd],
-                                    window_name="ply2", width=800, height=800,
-                                    front=[0.40, -0.20, -0.80],
-                                    lookat=[2.6172, 2.0475, 1.532],
-                                    up=[-0.0694, +0.9768, 0.2024],
-                                    zoom=0.24
+                                    window_name="ply2", width=400, height=400,
+                                    front=[-0.0, +0.01020, -0.080],
+                                    lookat=[0, 0, 10],
+                                    up=[+10.20694, 0, 00],
+                                    zoom=0.04
                                     )
 
 def show_voxel(pcd):
@@ -30,23 +31,61 @@ def show_voxel(pcd):
     downpcd = pcd.voxel_down_sample(voxel_size=0.05)
     o3d.visualization.draw_geometries([downpcd],window_name="voxel",
                                     width=800, height=800,
-                                    front=[0.4257, -0.2125, -0.8795],
+                                    front=[-25.4257, -0.2125, -0.8795],
                                     lookat=[2.6172, 2.0475, 1.532],
                                     up=[-0.0694, -0.9768, 0.2024],
                                     zoom=0.3412,                                   )
 
-print("Load a ply point cloud, print it, and render it")
+
+def print_image(pcd):
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(visible = True) 
+    vis.add_geometry(pcd)
+    #is.get_render_option().load_from_json("Imaging/display/RenderOption.json")
+    ctr = vis.get_view_control()
+    ctr.set_zoom(0.4)
+    ctr.set_front([-0.084777469999256949, -0.30273583588141922, -0.94929647331784794])
+    ctr.set_lookat([0.0,0.0,26.0])
+    ctr.set_up([+10.20694, 0, 00])
+    vis.run()
+    img = vis.capture_screen_float_buffer(True)
+    plt.imshow(np.asarray(img))
+    vis.capture_screen_image("ud.png", do_render=True)
+    #vis.capture_screen_image("ud1.png", do_render=False)
+
+def pcl2png(infilename, outfilename):
+    pcd1 = o3d.io.read_point_cloud(str(infilename))
+    vis = o3d.visualization.Visualizer()
+    vis.create_window(visible = False) 
+    vis.add_geometry(pcd)
+    #is.get_render_option().load_from_json("Imaging/display/RenderOption.json")
+    ctr = vis.get_view_control()
+    ctr.set_zoom(0.4)
+    ctr.set_front([-0.084777469999256949, -0.30273583588141922, -0.94929647331784794])
+    ctr.set_lookat([0.0,0.0,26.0])
+    ctr.set_up([+10.20694, 0, 00])
+    #vis.run()
+    img = vis.capture_screen_float_buffer(True)
+    plt.imshow(np.asarray(img))
+    vis.capture_screen_image(str(outfilename), do_render=True)
+
+
+
 TESTDATA = Path(file)
 pcd1 = o3d.io.read_point_cloud(str(TESTDATA))
 #textured_mesh = o3d.io.read_triangle_mesh(str(Path(file2)))
 #3print(pcd)
 #print(np.asarray(pcd.points))
 
-show(pcd1)
+#show(pcd1)
 
 show2(pcd1)
 
+print_image(pcd1)
 
+# o3d.visualization.capture_screen_image(pcd1,"ud.png", do_render=True)
+
+# o3d.visualization.capture_screen_image(pcd1,"ud2.png", do_render=False)
 #show_voxel(pcd1)
 
 #show(textured_mesh)
